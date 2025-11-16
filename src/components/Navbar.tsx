eb7d1efced0babbe77bf1628d40b837aa03d25ee
Ahmad Salah <ahmed.s@makla.ca>
2025-11-16 20:44:17 +0200

    fixed drawer for md screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
         keepMounted: true, // Better open performance on mobile.
       }}
       sx={{
-        display: { xs: "block", sm: "none" },
+        display: { xs: "block", md: "none" },
         "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
         backdropFilter: "blur(20px)",
       }}
